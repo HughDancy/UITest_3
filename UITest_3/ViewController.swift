20ffc2c0213c712b47185a4a64ee22dc0f3a5563
BorisKiselev <hugh.dancy@icloud.com>
2022-05-11 22:57:47 +0700

    Initial Commit

---
+++ UITest_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITest_3
+//
+//  Created by Борис Киселев on 11.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
