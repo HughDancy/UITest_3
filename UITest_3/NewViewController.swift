8ee13eeef201758910f83683a39207e9765d66e2
BorisKiselev <hugh.dancy@icloud.com>
2022-05-14 12:25:21 +0700

    Setup view's configuration #1
    Частично допилен интерфейс таска

--- UITest_3/NewViewController.swift
+++ UITest_3/NewViewController.swift
@@ -5,4 +5,25 @@
 //  Created by Борис Киселев on 11.05.2022.
 //
 
-import Foundation
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var button: UIButton!
+    @IBAction func buttonAction(_ sender: Any) {
+        if textField.hasText == true {
+            label.text = textField.text
+            textField.text = ""
+        }
+    }
+    
+}
